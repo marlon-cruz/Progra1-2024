40e375ef3587135c22391adccd2def86719b69fb
Marlon <marlo@MARLON>
2024-08-13 08:33:50 -0600

    Ejercicio de impuestos de clase

--- Proyecto1/Proyecto1/Program.cs
+++ Proyecto1/Proyecto1/Program.cs
@@ -6,32 +6,39 @@ using System.Threading.Tasks;
 
 namespace Proyecto1
 {
-    class Program
+   internal class Program
     {
         static void Main(string[] args)
         {
-            //Uso de matrices.
-            //Uso de matrices.
-            int[] numeros = new int[] { 2, 8, 9, 10, 1, 3 };
-            for (int i = 0; i < numeros.Length; i++)
-            {
-                Console.WriteLine("Indice: {0}={1}", i, numeros[i]);
-                int[,] matriz = new int[,] {
-                {5,4,5,2,1},
-                {1,2,8,9,3}
+            //Uso de matrices
+            double[,] matriz = new double[,] {
+                {0.01, 487.60, 0, 0},
+                {487.61, 642.85, 10, 17.48},
+                {642.86, 915.81, 10, 32.70},
+                {915.82, 2058.67, 20, 60.00 },
+                {2058.68, 9999999, 30, 288.57},
             };
-                for (int n = 0; n < 2; n++)
-                {
-                    for (int j = 0; j < 5; j++)
-                    {
-                        Console.Write("{0} ", matriz[n, j]);
-                    }
-                    Console.WriteLine("\n");
-                }
-                //Pausa
-                Console.ReadKey();
-            }
-        }
+            Console.Write("Sueldo: ");
+            double sueldo = double.Parse(Console.ReadLine()),
+                afp = sueldo * 6.25 / 100,
+                isss = sueldo * 3 / 100;
+            sueldo -= afp;
 
+            double isr = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                if (sueldo >= matriz[i, 0] && sueldo <= matriz[i, 1])
+                {
+                    isr = (sueldo - (matriz[i, 0] - 0.01)) * matriz[i, 2] / 100 + matriz[i, 3];
+                }
+            }
+
+            sueldo -= isss;
+            sueldo -= isr;
+            Console.WriteLine("Sueldo Neto: {0}, AFP: {1}, ISSS: {2}, ISR: {3}", Math.Round(sueldo, 2), afp, isss, Math.Round(isr, 2));
+
+            Console.ReadLine();
+        }
+    
     }
 }
