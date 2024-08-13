7b3f9bd458731338340154759e1691276712c740
Marlon <marlo@MARLON>
2024-08-13 08:02:03 -0600

    Uso de matriz bidimensional

--- Proyecto1/Proyecto1/Program.cs
+++ Proyecto1/Proyecto1/Program.cs
@@ -10,17 +10,28 @@ namespace Proyecto1
     {
         static void Main(string[] args)
         {
-            //Uso de matriz
-            int[] numeros = new int[] { 2, 8, 9, 10,18, 1, 3 };
-            //Mostrar contenido de una posicion en la patriz
-            for(int i = 0; i < numeros.Length; i++)
+            //Uso de matrices.
+            //Uso de matrices.
+            int[] numeros = new int[] { 2, 8, 9, 10, 1, 3 };
+            for (int i = 0; i < numeros.Length; i++)
             {
-                Console.WriteLine("Indice: {0} = {1}", i, numeros[i]);
+                Console.WriteLine("Indice: {0}={1}", i, numeros[i]);
+                int[,] matriz = new int[,] {
+                {5,4,5,2,1},
+                {1,2,8,9,3}
+            };
+                for (int n = 0; n < 2; n++)
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+                        Console.Write("{0} ", matriz[n, j]);
+                    }
+                    Console.WriteLine("\n");
+                }
+                //Pausa
+                Console.ReadKey();
             }
-
-            //Pausa
-            Console.Read();
         }
-    }
 
+    }
 }
