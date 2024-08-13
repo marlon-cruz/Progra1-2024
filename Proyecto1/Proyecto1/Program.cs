ceccf9806d750fae0563735d10df5c49422c4951
Marlon <marlo@MARLON>
2024-08-13 07:39:51 -0600

    Uso de matriz con for

--- Proyecto1/Proyecto1/Program.cs
+++ Proyecto1/Proyecto1/Program.cs
@@ -13,7 +13,10 @@ namespace Proyecto1
             //Uso de matriz
             int[] numeros = new int[] { 2, 8, 9, 10,18, 1, 3 };
             //Mostrar contenido de una posicion en la patriz
-            Console.WriteLine("El numero de la posicion 3 es: {0}", numeros[3]);
+            for(int i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine("Indice: {0} = {1}", i, numeros[i]);
+            }
 
             //Pausa
             Console.Read();
