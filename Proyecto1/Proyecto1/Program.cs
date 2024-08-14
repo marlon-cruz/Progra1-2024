1ec17673c94c5f88661462b52406a521e721bd69
Marlon <marlo@MARLON>
2024-08-14 09:45:42 -0600

    Desarrojo de ejercicio de SECUENCIA FIBONACCI

--- Proyecto1/Proyecto1/Program.cs
+++ Proyecto1/Proyecto1/Program.cs
@@ -10,32 +10,32 @@ namespace Proyecto1
     {
         static void Main(string[] args)
         {
-            //Uso de matrices
-            double[,] matriz = new double[,] {
-                {0.01, 472.60, 0, 0},
-                {472.01, 895.24, 10, 17.67},
-                {895.25, 2038.24, 20, 60.00},
-                {2038.25, 9999999, 30, 288.57 },
-               
-            };
-            Console.Write("Sueldo: ");
-            double sueldo = double.Parse(Console.ReadLine()),
-                afp = sueldo * 7.25 / 100,
-                isss = sueldo * 3 / 100,
-                isr = 0;
-            sueldo -= afp;
 
-            for (int i = 0; i < 4; i++)
+            //SECUENCIA FIBONACCI
+            /*
+             0+1 = 1
+             1+0 = 1
+             1+1 = 2
+             2+1 = 3
+             3+2 = 5
+             5+3 = 8
+             8+5 = 13
+             */
+            int a = 0, b = 1, c = 1;
+            while (c < 100)
             {
-                if (sueldo >= matriz[i, 0] && sueldo <= matriz[i, 1])
-                {
-                    isr = (sueldo - (matriz[i, 0] - 0.01)) * matriz[i, 2] / 100 + matriz[i, 3];
-                }
+                Console.WriteLine("{0} + {1} = {2}", a, b, c);
+                
+                b = a;
+                a = c;
+                c = a + b;
+                
+               
             }
+        
 
-            sueldo -= isss;
-            sueldo -= isr;
-            Console.WriteLine("Sueldo Neto: {0}, AFP: {1}, ISSS: {2}, ISR: {3}", Math.Round(sueldo, 2), afp, isss, Math.Round(isr, 2));
+
+
 
             Console.ReadLine();
         }
