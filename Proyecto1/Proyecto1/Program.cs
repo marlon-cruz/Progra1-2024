1ec710333992aed43ae9d612da7e8954870184b8
Marlon <marlo@MARLON>
2024-08-20 08:38:44 -0600

    ejercicio encontrar la media aritmetica en la desviacion tipica de una serie de numeros mas la media armonica

--- Proyecto1/Proyecto1/Program.cs
+++ Proyecto1/Proyecto1/Program.cs
@@ -6,43 +6,59 @@ using System.Threading.Tasks;
 
 namespace Proyecto1
 {
-   internal class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
             //Uso de funcionesy matrices
-            //ejercicio encontrar la media aritmetica en la desviacion tipica de una serie de numeros
-
-            int[] serie = new int[] {1,2,3,4,5};
+            //ejercicio encontrar la media aritmetica en la desviacion tipica de una serie de numeros mas la media armonica
 
 
-            Console.WriteLine("La media aritmetca es: {0} y la desviacion tipica es: {1}",media(serie),tipica(serie));
+            Console.Write("Ingrese la serie de numeros separados por comas: ");
+            String serie = Console.ReadLine();
+            String[] numeros = serie.Split(',');
+
+            Console.WriteLine("La media aritmetica es: {0}, y la desviacion tipica es: {1} ademas la media armonica es: {2} ", media(numeros), tipica(numeros),media_A(numeros));
             Console.Read();
         }
-      
-        static double media(int[] serie)
+
+        static double media(string[] serie)
         {
             double media = 0;
-            foreach (int num in serie)
+            foreach (string num in serie)
             {
-                media += num;
+                media += int.Parse(num);
 
             }
 
             return media / serie.Length;
 
         }
-        static double tipica(int[] serie)
+        static double tipica(string[] serie)
         {
-            double tipica = 0;
-            double m = media(serie);
-            foreach(int num in serie)
+            double tipica = 0,
+                   m = media(serie);
+            foreach (string num in serie)
             {
-                tipica += Math.Pow(num - m, 2);
+                tipica += Math.Pow(int.Parse(num) - m, 2);
             }
             tipica = Math.Sqrt(tipica / serie.Length);
-            return tipica;
 
+            return Math.Round(tipica, 2);
+
+        }
+        static double media_A(string[] serie)
+        {
+            int n = serie.Length;
+            double armonica = 0;
+           
+            foreach(string num in serie)
+            {
+                armonica += 1 / int.Parse(num);
+            }
+
+            armonica = n / armonica;
+            return armonica;
         }
     }
 }
