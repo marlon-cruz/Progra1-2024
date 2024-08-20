85cb7d27501cdc359f1c48e6fc8c5d511ee489b9
Marlon <marlo@MARLON>
2024-08-20 07:13:17 -0600

    Uso de funciones

--- Proyecto1/Proyecto1/Program.cs
+++ Proyecto1/Proyecto1/Program.cs
@@ -10,28 +10,17 @@ namespace Proyecto1
     {
         static void Main(string[] args)
         {
-            //determinar si un numero es primo
+            //Uso de funciones
 
-            int[] serie = new int[] { 5, 9, 4, 6, 3, 2,7,11,15,17,21 };
-            foreach (int num in serie)
-            {
-                int i = 1;
-                int n = 0;
-                while(i <= num && n <= 2)
-                {
-                    if (num % i == 0)
-                    {
-                        n++;
-                       
-                    }
-                    i++;
-                }
-                Console.WriteLine("El numero {0} es {1}", num, n <= 2 ? "Es primo" : "No es primo");
-            }
+            Console.WriteLine("El resultado de la suma es: {0}", sumar());
 
             Console.Read();
-
-           }
+        }
+       static int sumar()
+        {
+            int respuesta = 10 + 5;
+            return respuesta;
+        }
 
     }
 }
