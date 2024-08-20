ebec0208696784b77ae658d9f5bce5cf54d0196b
Marlon <marlo@MARLON>
2024-08-20 07:21:16 -0600

    uso de funciones con parametros

--- Proyecto1/Proyecto1/Program.cs
+++ Proyecto1/Proyecto1/Program.cs
@@ -14,11 +14,23 @@ namespace Proyecto1
 
             Console.WriteLine("El resultado de la suma es: {0}", sumar());
 
+            Console.Write("Num 1: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Num 2: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("La suma de {0} + {1} = {2}", num1, num2, sumar(num1, num2));
+
             Console.Read();
         }
        static int sumar()
         {
             int respuesta = 10 + 5;
+            return respuesta;
+        }
+        static int sumar(int a, int b)
+        {
+            int respuesta = a +b;
             return respuesta;
         }
 
