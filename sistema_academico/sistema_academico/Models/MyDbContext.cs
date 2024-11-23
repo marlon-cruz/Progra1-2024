4b5865b8e3b80318cdcaf776f57ef4143f1cb04d
Marlon <marlo@MARLON>
2024-11-23 07:55:24 -0600

    Creacion de formularios (matricula) avanzados maestro detalle

--- sistema_academico/sistema_academico/Models/MyDbContext.cs
+++ sistema_academico/sistema_academico/Models/MyDbContext.cs
@@ -1,8 +1,8 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
 
 namespace sistema_academico.Models
 {
@@ -13,13 +13,13 @@ namespace sistema_academico.Models
         public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
         public DbSet<Alumno> Alumnos { get; set; }
         public DbSet<Docente> Docentes { get; set; }
+        public DbSet<Matricula> Matriculas { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Alumno>().HasKey(e => e.idAlumno);
-
             modelBuilder.Entity<Docente>().HasKey(e => e.idDocente);
-
+            modelBuilder.Entity<Matricula>().HasKey(e => e.idMatricula);
         }
     }
 }
